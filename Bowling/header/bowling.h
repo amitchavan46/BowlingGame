9d21b45be7f21b475f8cdf9a1f3f69437ea31f4f
amitchavan46 <amitchavan462002@gmail.com>
2025-05-16 23:38:44 +0530

    Add files via upload
    Bowling Score Calculator files added

---
+++ Bowling/header/bowling.h
@@ -0,0 +1,22 @@
+// bowling_game.h
+#ifndef BOWLING_H
+#define BOWLING_H
+
+#include <vector>
+
+class BowlingGame {
+public:
+    void roll(int pins);
+    int score();
+
+private:
+    std::vector<int> rolls;
+
+    bool isStrike(int rollIndex);
+    bool isSpare(int rollIndex);
+    int strikeBonus(int rollIndex);
+    int spareBonus(int rollIndex);
+    int framePoints(int rollIndex);
+};
+
+#endif // BOWLING_H
---
+++ Bowling/src/bowling.cpp
@@ -0,0 +1,46 @@
+// bowling_game.cpp
+#include "bowling.h"
+
+void BowlingGame::roll(int pins) {
+    rolls.push_back(pins);
+}
+
+int BowlingGame::score() {
+    int totalScore = 0;
+    int rollIndex = 0;
+
+    for (int frame = 0; frame < 10; ++frame) {
+        if (isStrike(rollIndex)) {
+            totalScore += 10 + strikeBonus(rollIndex);
+            rollIndex += 1;
+        } else if (isSpare(rollIndex)) {
+            totalScore += 10 + spareBonus(rollIndex);
+            rollIndex += 2;
+        } else {
+            totalScore += framePoints(rollIndex);
+            rollIndex += 2;
+        }
+    }
+
+    return totalScore;
+}
+
+bool BowlingGame::isStrike(int rollIndex) {
+    return rolls[rollIndex] == 10;
+}
+
+bool BowlingGame::isSpare(int rollIndex) {
+    return rolls[rollIndex] + rolls[rollIndex + 1] == 10;
+}
+
+int BowlingGame::strikeBonus(int rollIndex) {
+    return rolls[rollIndex + 1] + rolls[rollIndex + 2];
+}
+
+int BowlingGame::spareBonus(int rollIndex) {
+    return rolls[rollIndex + 2];
+}
+
+int BowlingGame::framePoints(int rollIndex) {
+    return rolls[rollIndex] + rolls[rollIndex + 1];
+}
